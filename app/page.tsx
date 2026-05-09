a8aa6c4b6516a916b86a013cbc2ce324a3fef134
reyezgcharlie-ux <reyezgcharlie@gmail.com>
2026-05-08 23:54:38 -0700

    Initial home page

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-6xl font-bold neon">Synapt Community Hub</h1>
+    </div>
+  );
+}
